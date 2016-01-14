b95bbe306ec02656beff02fe5fa25c4420eddc54
lijinliang062034 <396220565@qq.com>
2016-01-14 10:12:58 +0800

    第一次提交 版本1.0

---
+++ TextForGitHub/TextForGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextForGitHub
+//
+//  Created by lanou3g on 16/1/14.
+//  Copyright © 2016年 蓝鸥科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextForGitHub/TextForGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextForGitHub
+//
+//  Created by lanou3g on 16/1/14.
+//  Copyright © 2016年 蓝鸥科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
